c7127b25412a1e9f36ab5cdff4b6ece5bc8ff1ae
Vinayak <vinayakcpa@gmail.com>
2023-05-27 19:30:08 -0400

    added in remaining necessary functions for weather.js

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 import "./style.css";
 import { getWeather } from "./weather";
 
-getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(
-  (data) => {
-    console.log(data);
-  }
-);
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone)
+  .then(renderWeather)
+  .catch((e) => {
+    console.error(e);
+    alert("Error getting weather.");
+  });
+
+function renderWeather({ current, daily, hourly }) {
+  renderCurrentWeather(current);
+  // renderDailyWeather(daily)
+  // renderHourlyWeather(hourly)
+  document.body.classList.remove("blurred");
+}
+
+function setValue(selector, value, { parent = document } = {}) {
+  parent.querySelector(`[data-${selector}]`).textContent = value;
+}
+
+function renderCurrentWeather(current) {
+  document.querySelector("[data-current-temp]").textContent =
+    current.currentTemp;
+}
