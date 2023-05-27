1470dd915e688d0b695101d53c32c6fba82fd116
Vinayak <vinayakcpa@gmail.com>
2023-05-27 17:51:03 -0400

    added in parseCurrentWeather function

--- weather.js
+++ weather.js
@@ -1,16 +1,48 @@
 import axios from "axios";
 
-//https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation_probability,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime
-
 export function getWeather(lat, lon, timezone) {
-  axios.get(
-    "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation_probability,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime",
-    {
-      params: {
-        latitude: lat,
-        longditude: lon,
-        timezone,
-      },
-    }
-  );
+  return axios
+    .get(
+      "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation_probability,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime",
+      {
+        params: {
+          latitude: lat,
+          longditude: lon,
+          timezone,
+        },
+      }
+    )
+    .then(({ data }) => {
+      return {
+        current: parseCurrentWeather(data),
+        daily: parseDailyWeather(data),
+        // hourly: parseHourlyWeather(data)
+      };
+    });
+}
+
+function parseCurrentWeather({ current_weather, daily }) {
+  const {
+    temperature: currentTemp,
+    windspeed: windSpeed,
+    weathercode: iconCode,
+  } = current_weather;
+  const {
+    temperature_2m_max: [maxTemp],
+    temperature_2m_min: [minTemp],
+    apparent_temperature_max: [maxFeelsLike],
+    apparent_temperature_minx: [minFeelsLike],
+    precipitation_sum: [precip],
+  } = daily;
+
+  return {
+    currentTemp: Math.round(currentTemp),
+    highTemp: Math.round(maxTemp),
+    lowTemp: Math.round(minTemp),
+    highFeelsLike: Math.round(maxFeelsLike),
+    lowFeelsLike: Math.round(minFeelsLike),
+    windSpeed: Math.round(windSpeed),
+    precip: Math.round(precip * 100) / 100,
+    iconCode,
+  };
 }
