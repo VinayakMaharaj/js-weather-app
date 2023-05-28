d049446633b61cca9c18d5bec12992ac056c67f5
Vinayak <vinayakcpa@gmail.com>
2023-05-27 20:03:51 -0400

    adding iconMap js file

--- main.js
+++ main.js
@@ -19,7 +19,18 @@ function setValue(selector, value, { parent = document } = {}) {
   parent.querySelector(`[data-${selector}]`).textContent = value;
 }
 
+function getIconUrl(iconCode) {
+  return `icons/${iconCode}.svg`;
+}
+
+const currentIcon = document.querySelector("[data-current-icon]");
 function renderCurrentWeather(current) {
-  document.querySelector("[data-current-temp]").textContent =
-    current.currentTemp;
+  currentIcon.src = getIconUrl(current.iconCode);
+  setValue("current-temp", current.currentTemp);
+  setValue("current-high", current.highTemp);
+  setValue("current-low", current.lowTemp);
+  setValue("current-fl-high", current.highFeelsLike);
+  setValue("current-fl-low", current.lowFeelsLike);
+  setValue("current-wind", current.windSpeed);
+  setValue("current-precip", current.precip);
 }
